6eddb07e4b650dd818a9f5cfb20a2d44c200172d
blissun <hello-ryan@kakao.com>
2021-02-04 22:35:20 +0900

    product.py : add aws environ

--- config/settings/product.py
+++ config/settings/product.py
@@ -18,3 +18,16 @@ DATABASES = {
         "PORT": 5432,
     }
 }
+
+
+DEFAULT_FILE_STORAGE = 'config.custom_storages.UploadStorage'
+STATICFILES_STORAGE = 'config.custom_storages.StaticStorage'
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+# AWS_S3_OBJECT_PARAMETERS = {}
+AWS_S3_CUSTOM_DOMAIN = "AWS_S3_CUSTOM_DOMAIN"
+AWS_DEFAULT_ACL = 'public-read'
+
+# AWS_S3_CUSTOM_DOMAIN = f"{os.environ.get('AWS_S3_CUSTOM_DOMAIN')}"
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
